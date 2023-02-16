ab1d6d5c1f27147b3783adc1d5c787c66cd3bd56
Luiz Gustavo <22282203@CFP127ALUNO.LOCAL>
2023-02-16 16:58:13 -0300

    Object add

--- Aula02/main.js
+++ Aula02/main.js
@@ -9,9 +9,8 @@ const adicionar = document.getElementById('adicionar')
 //     const container = document.getElementById('container')
 //     container.innerHTML = container.innerHTML + '<div class="item">Senai</div>'
 // }
-
-const handleClick = () => {
-    const nome = prompt('Digite o nome do Aluno: ')
+const verificarNome = (nomeVar) => {
+    const nome = nomeVar
 
     if(nome == ''){
         alert('Digite algum nome')
@@ -20,32 +19,44 @@ const handleClick = () => {
     }else if(/[0-9]/.test(nome)){
         alert('Digite nome sem número')
     }else{
-        const nota = prompt('Digite a nota do Aluno: ').replace(',','.')
-        if(nota == 0){
-            alert('Digite alguma nota')
-        }else if(nota == null){
-            alert('Digite alguma nota')
-        }else if(isNaN(nota)){
-            alert('Digite uma nota válida')
-        }else if (nota < 0 || nota > 10){
-            alert('Digite uma nota válida, entre 0 e 10')
-        }else{
-            adicionarCard(nome, nota)
-        }
+        return nome
     }
 }
-const adicionarCard = (nome, nota) => {
-    const nomeVar = nome
-    const notaVar = nota
+
+const verificarNota = (notaVar) => {
+    const nota = notaVar
+
+    if(nota == 0){
+        alert('Digite alguma nota')
+    }else if(nota == null){
+        alert('Digite alguma nota')
+    }else if(isNaN(nota)){
+        alert('Digite uma nota válida')
+    }else if (nota < 0 || nota > 10){
+        alert('Digite uma nota válida, entre 0 e 10')
+    }else{
+        return nota
+    }
+}
+
+const handleClick = () => {
+    const aluno = {
+        nome: verificarNome(prompt('Digite o nome do Aluno: ')),
+        nota: verificarNota(prompt('Digite o nota do Aluno: ').replace(',','.'))
+    }
+        adicionarCard(aluno)
+}
+
+const adicionarCard = (aluno) => {
     const container = document.getElementById('container')
     const novaDiv = document.createElement('div')
-    if(notaVar < 6){
+    if(aluno.nota < 6){
         novaDiv.classList.add('aluno__reprovado')
     }else {
         novaDiv.classList.add('aluno__aprovado')
     }
-    novaDiv.innerHTML = `<h2 class = "aluno__nome">${nomeVar}</h2>
-                         <span class = "aluno__nota">${notaVar}</span>`
+    novaDiv.innerHTML = `<h2 class = "aluno__nome">${aluno.nome}</h2>
+                         <span class = "aluno__nota">${aluno.nota}</span>`
     container.appendChild(novaDiv)
 }
 
